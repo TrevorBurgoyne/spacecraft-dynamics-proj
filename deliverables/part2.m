7b88a532d607c90b431e56b37cfa2b21f95cf066
TrevorBurgoyne <trevorburgoyne@gmail.com>
2023-02-25 15:27:28 -0600

    Part 2 deliverables

---
+++ deliverables/part2.m
@@ -0,0 +1,9 @@
+%% Part 2 Code Deliverables
+% Trevor Burgoyne, 3 Mar 2023,
+% AEM 4305 Spacecraft Attitude Dynamics and Control
+
+% Add path to simulation functions so that we can use them
+addpath  '..\simulation\';
+
+%% Run Simulation (with J2)
+run_simulation
