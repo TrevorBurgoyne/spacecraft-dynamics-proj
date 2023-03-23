353a8c67f0281c5f8eb65decca295d57d8509078
TrevorBurgoyne <trevorburgoyne@gmail.com>
2023-03-22 19:13:01 -0500

    Update initial conditions, use odewar

--- simulation/run_simulation.m
+++ simulation/run_simulation.m
@@ -1,7 +1,7 @@
 %% Spacecraft Simulation
 % Created by James Richard Forbes
 % Edited by Ryan James Caverly, Trevor Burgoyne
-% Updated 17 Mar 2023
+% Updated 24 Mar 2023
 
 clear all
 format long
@@ -9,25 +9,27 @@ format long
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Constants 
 addpath  '..\simulation\utils\'; % Add util functions to path
+addpath  '..\simulation\odewbar'; % function to show wait bar while running sim
 const_struct % All constants in one file.
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% Initial conditions.
-% x_0    = [0, 0, C.Rs]';    % Initial position [x,y,z]'
-% x_dot0 = [0, C.v, 0]';     % Initial velocity [xdot,ydot,zdot]'
-% x      = [x_0'; x_dot0']'; % Initial Conditions
-
+% Initial Conditions
+% Translational
+r_0    = [0, 0, C.Rs]';    % Initial position [x,y,z]'
+v_0    = [0, C.v, 0]';     % Initial velocity [xdot,ydot,zdot]'
 % Rotational
 epsilon_0 = zeros(3,1);        % Initial epsilon
 eta_0     = 1;                 % Initial eta
 omega_0   = [0.08; -0.1; 0.3]; % Initial omega (rad/s)
-x         = [epsilon_0', eta_0, omega_0']'; % Initial Conditions
+
+% Combined Initial Conditions
+x = [r_0', v_0', epsilon_0', eta_0, omega_0']'; 
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Simulation time.
 t_0 = 0; % s
 % t_max = .75*C.day; % s
-t_max = 2*C.min; % s
+t_max = C.hour; % s
 t_div = 10001; % number of steps to divide the time series into.
 t_span = linspace(t_0,t_max,t_div); % Total simulation time.
 
@@ -37,7 +39,8 @@ options = odeset('AbsTol',1e-9,'RelTol',1e-9); % This changes the integration to
 
 tic
 % [t,x_out] = ode45(@TranslationalODEs,t_span,x,options);
-[t,x_out] = ode45(@RotationalODEs,t_span,x,options);
+% [t,x_out] = ode45(@RotationalODEs,t_span,x,options);
+[t,x_out] = ode45(@ODEs,t_span,x,options,'OutputFcn',@odewbar);
 time_stamp = toc;
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
