4220b0c5d5f7d63e4a052bacf5f5fddc6d466fa9
TrevorBurgoyne <trevorburgoyne@gmail.com>
2023-03-22 19:13:21 -0500

    Plot/process all the things

--- simulation/plot_script_v2.m
+++ simulation/plot_script_v2.m
@@ -1,7 +1,7 @@
 %% Plotting script.
 % Created by James Richard Forbes
 % Edited by Ryan James Caverly, Trevor Burgoyne
-% Updated 17 Mar 2023
+% Updated 24 Mar 2023
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Font size, line size, and line width. 
@@ -14,66 +14,66 @@ line_width = 2;
 % Notice how the axes for each plot are labeled with the appropriate units
 
 % System response
-% figure('Position',[100 100 1200 800])
-% % x vs time
-% subplot(2,3,1) 
-% plot(t,x_out(:,1),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('x (m)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
-% 
-% % y vs time
-% subplot(2,3,2) 
-% plot(t,x_out(:,2),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
-% 
-% % z vs time
-% subplot(2,3,3) 
-% plot(t,x_out(:,3),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('z (m)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
-% 
-% % xdot vs time
-% subplot(2,3,4)
-% plot(t,x_out(:,4),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('xdot (m/s)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
-% 
-% % ydot vs time
-% subplot(2,3,5)
-% plot(t,x_out(:,5),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('ydot (m/s)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
-% 
-% % xzdot vs time
-% subplot(2,3,6)
-% plot(t,x_out(:,6),'Linewidth',line_width);
-% hold on
-% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-% ylabel('zdot (m/s)','fontsize',font_size,'Interpreter','latex');
-% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-% grid on
-% axis square
+figure('Position',[100 100 1200 800])
+% x vs time
+subplot(2,3,1) 
+plot(t,x_out(:,1),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('x (m)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
+
+% y vs time
+subplot(2,3,2) 
+plot(t,x_out(:,2),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
+
+% z vs time
+subplot(2,3,3) 
+plot(t,x_out(:,3),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('z (m)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
+
+% xdot vs time
+subplot(2,3,4)
+plot(t,x_out(:,4),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('xdot (m/s)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
+
+% ydot vs time
+subplot(2,3,5)
+plot(t,x_out(:,5),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('ydot (m/s)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
+
+% zdot vs time
+subplot(2,3,6)
+plot(t,x_out(:,6),'Linewidth',line_width);
+hold on
+xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+ylabel('zdot (m/s)','fontsize',font_size,'Interpreter','latex');
+set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+grid on
+axis square
 
 % Rotational stuffs
 % epsilon1
@@ -210,15 +210,15 @@ ylabel('$(E - E(0))/E(0)$ (J)','fontsize',font_size,'Interpreter','latex');
 set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
 grid on
 
-% % Plot of spacecraft trajectory around Earth
-% figure
-% plot3(x_out(:,1),x_out(:,2),x_out(:,3))
-% xlabel('x (m)','fontsize',font_size,'Interpreter','latex');
-% ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
-% zlabel('z (m)','fontsize',font_size,'Interpreter','latex');
-% grid on
-% axis equal
-% hold on
-% [X,Y,Z] = sphere; % Sphere
-% surf(X*C.Re, Y*C.Re, Z*C.Re); % Earth
+% Plot of spacecraft trajectory around Earth
+figure
+plot3(x_out(:,1),x_out(:,2),x_out(:,3))
+xlabel('x (m)','fontsize',font_size,'Interpreter','latex');
+ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
+zlabel('z (m)','fontsize',font_size,'Interpreter','latex');
+grid on
+axis equal
+hold on
+[X,Y,Z] = sphere; % Sphere
+surf(X*C.Re, Y*C.Re, Z*C.Re); % Earth
 
--- simulation/post_processing_v2.m
+++ simulation/post_processing_v2.m
@@ -1,17 +1,17 @@
 %% Post processing script.
 % Created by James Richard Forbes
 % Edited by Ryan James Caverly, Trevor Burgoye
-% Updated 17 Mar 2023
+% Updated 24 Mar 2023
 
 addpath ..\simulation\utils\ % Add util functions to path
 const_struct   % Extract C (constants) struct from utils\const_struct.m 
 
 % Pull data from global vars
-% x     = x_out(:,1:3);
-% x_dot = x_out(:,4:6);
-epsilon = x_out(:,1:3)';
-eta     = x_out(:,4);
-omega   = x_out(:,5:7)';
+x       = x_out(:,1:3);
+x_dot   = x_out(:,4:6);
+epsilon = x_out(:,7:9)';
+eta     = x_out(:,10);
+omega   = x_out(:,11:13)';
 
 
 % Here is where you do post processing. 
@@ -22,7 +22,7 @@ omega   = x_out(:,5:7)';
 n_entries = length(omega);
 
 % Energy
-E = zeros(n_entries,1);
+T_r = zeros(n_entries,1);
 
 % Euler Angles
 yaw   = zeros(n_entries,1);
@@ -32,9 +32,12 @@ roll  = zeros(n_entries,1);
 % Quaternion Norm
 q_norm = zeros(n_entries,1);
 
+% Gravity Gradient Torque
+V_gg = zeros(n_entries,1);
+
 for i=1:n_entries
     % Energy
-    E(i) = .5*omega(:,i)'*C.I*omega(:,i);
+    T_r(i) = .5*omega(:,i)'*C.I*omega(:,i);
     
     % Euler Angles
     q = [epsilon(:,i); eta(i)];
@@ -46,16 +49,24 @@ for i=1:n_entries
 
     % Quaternion Unit Norm Check
     q_norm(i) = epsilon(:,i)'*epsilon(:,i) + eta(i)^2 - 1;
+    
+    % r_b
+    r_b = C_ba*x(i);
+    r = norm(r_b);
+    
+    % Gravity Gradient Torque
+    V_gg(i) = (.5*C.mu*r^-3)*((3*r^-2)*crossm(r_b)'*C.I*r_b  - trace(C.I));
 end
 
-% % Translational
-% v = vecnorm(x_dot,2,2); % Magnitude of xdot at each time tick
-% r = vecnorm(x,2,2);     % Magnitude of x at each time tick
-% 
-% T  = .5*C.ms*(v.^2);     % KE at each time tick
-% V  = -(C.mu*C.ms)./r;    % PE at each time tick
-% 
-% % J2 PE Correction
-% J2 = ((C.mu*C.ms*C.J2*C.Re^2)./(r.^3)).*( (3./(2*r.^2)).*(x(:,3).^2) -.5 );
-% 
-% E = T + V + J2;
+% Translational
+v = vecnorm(x_dot,2,2); % Magnitude of xdot at each time tick
+r = vecnorm(x,2,2);     % Magnitude of x at each time tick
+
+T  = .5*C.ms*(v.^2);     % KE at each time tick
+V_g  = -(C.mu*C.ms)./r;    % PE at each time tick
+
+% J2 PE Correction
+J2 = ((C.mu*C.ms*C.J2*C.Re^2)./(r.^3)).*( (3./(2*r.^2)).*(x(:,3).^2) -.5 );
+
+% Total energy
+E = T + T_r + V_g + J2 + V_gg;
