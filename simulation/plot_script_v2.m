1cd0c3c25f055d52cf31fad20d63eadb30903ed5
TrevorBurgoyne <trevorburgoyne@gmail.com>
2023-04-29 13:25:46 -0500

    Run sim for PD control using true attitude

--- simulation/plot_script_v2.m
+++ simulation/plot_script_v2.m
@@ -1,7 +1,7 @@
 %% Plotting script.
 % Created by James Richard Forbes
 % Edited by Ryan James Caverly, Trevor Burgoyne
-% Updated 13 Apr 2023
+% Updated 29 Apr 2023
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Font size, line size, and line width. 
@@ -14,66 +14,66 @@ line_width = 2;
 % Notice how the axes for each plot are labeled with the appropriate units
 
 % System response
-figure('Position',[100 100 1200 800])
-% x vs time
-subplot(2,3,1) 
-plot(t,x_out(:,1),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('x (m)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% y vs time
-subplot(2,3,2) 
-plot(t,x_out(:,2),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% z vs time
-subplot(2,3,3) 
-plot(t,x_out(:,3),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('z (m)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% xdot vs time
-subplot(2,3,4)
-plot(t,x_out(:,4),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('xdot (m/s)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% ydot vs time
-subplot(2,3,5)
-plot(t,x_out(:,5),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('ydot (m/s)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% zdot vs time
-subplot(2,3,6)
-plot(t,x_out(:,6),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('zdot (m/s)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
+% figure('Position',[100 100 1200 800])
+% % x vs time
+% subplot(2,3,1) 
+% plot(t,x_out(:,1),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('x (m)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % y vs time
+% subplot(2,3,2) 
+% plot(t,x_out(:,2),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % z vs time
+% subplot(2,3,3) 
+% plot(t,x_out(:,3),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('z (m)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % xdot vs time
+% subplot(2,3,4)
+% plot(t,x_out(:,4),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('xdot (m/s)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % ydot vs time
+% subplot(2,3,5)
+% plot(t,x_out(:,5),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('ydot (m/s)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % zdot vs time
+% subplot(2,3,6)
+% plot(t,x_out(:,6),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('zdot (m/s)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
 
 % Rotational stuffs
 % epsilon1
@@ -117,15 +117,15 @@ set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
 grid on
 axis square
 
-% q_norm
-subplot(2,4,5)
-plot(t,q_norm,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\varepsilon^{T}\varepsilon+\eta^{2}-1$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
+% % q_norm
+% subplot(2,4,5)
+% plot(t,q_norm,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\varepsilon^{T}\varepsilon+\eta^{2}-1$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
 
 % yaw
 subplot(2,4,6)
@@ -191,148 +191,148 @@ grid on
 axis square
 
 
-% Energy, which should be constant when damping is zero.
-figure
-subplot(2,1,1) % Plot of total energy vs time (should be constant)
-plot(t,E,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$E$ (J)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-
-% Plot of normalized change in energy vs time (should be zero)
-subplot(2,1,2)
-plot(t,(E-E(1))./E(1),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$(E - E(0))/E(0)$ (J)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-
-% Plot of spacecraft trajectory around Earth
-figure
-plot3(x_out(:,1),x_out(:,2),x_out(:,3))
-xlabel('x (m)','fontsize',font_size,'Interpreter','latex');
-ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
-zlabel('z (m)','fontsize',font_size,'Interpreter','latex');
-grid on
-axis equal
-hold on
-[X,Y,Z] = sphere; % Sphere
-surf(X*C.Re, Y*C.Re, Z*C.Re); % Earth
-
-
-% INS Estimates
-% epsilon1_hat
-figure('Position',[100 100 1200 600])
-subplot(2,4,1)
-plot(t,x_out(:,14),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\hat{\varepsilon}_{1}$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% epsilon2_hat
-subplot(2,4,2)
-plot(t,x_out(:,15),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\hat{\varepsilon}_{2}$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% epsilon3_hat
-subplot(2,4,3)
-plot(t,x_out(:,16),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\hat{\varepsilon}_{3}$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% eta_hat
-subplot(2,4,4)
-plot(t,x_out(:,17),'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\hat{\eta}$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% q_hat_norm
-subplot(2,4,5)
-plot(t,q_hat_norm,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('$\hat{\varepsilon}^{T}\hat{\varepsilon}+\hat{\eta}^{2}-1$','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% yaw_IN_err
-subplot(2,4,6)
-plot(t,yaw_IN_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('INS Yaw Error ($\psi^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% pitch_IN_err
-subplot(2,4,7)
-plot(t,pitch_IN_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('INS Pitch Error ($\theta^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% roll_IN_err
-subplot(2,4,8)
-plot(t,roll_IN_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('INS Roll Error ($\phi^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% TRIAD errors
-% yaw_TRIAD_err
-figure('Position',[100 100 1200 600])
-subplot(1,3,1)
-plot(t,yaw_TRIAD_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('TRIAD Yaw Error ($\psi^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% pitch_TRIAD_err
-subplot(1,3,2)
-plot(t,pitch_TRIAD_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('TRIAD Pitch Error ($\theta^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
-
-% roll_TRIAD_err
-subplot(1,3,3)
-plot(t,roll_TRIAD_err,'Linewidth',line_width);
-hold on
-xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
-ylabel('TRIAD Roll Error ($\phi^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
-set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
-grid on
-axis square
+% % Energy, which should be constant when damping is zero.
+% figure
+% subplot(2,1,1) % Plot of total energy vs time (should be constant)
+% plot(t,E,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$E$ (J)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% 
+% % Plot of normalized change in energy vs time (should be zero)
+% subplot(2,1,2)
+% plot(t,(E-E(1))./E(1),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$(E - E(0))/E(0)$ (J)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% 
+% % Plot of spacecraft trajectory around Earth
+% figure
+% plot3(x_out(:,1),x_out(:,2),x_out(:,3))
+% xlabel('x (m)','fontsize',font_size,'Interpreter','latex');
+% ylabel('y (m)','fontsize',font_size,'Interpreter','latex');
+% zlabel('z (m)','fontsize',font_size,'Interpreter','latex');
+% grid on
+% axis equal
+% hold on
+% [X,Y,Z] = sphere; % Sphere
+% surf(X*C.Re, Y*C.Re, Z*C.Re); % Earth
+% 
+% 
+% % INS Estimates
+% % epsilon1_hat
+% figure('Position',[100 100 1200 600])
+% subplot(2,4,1)
+% plot(t,x_out(:,14),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\hat{\varepsilon}_{1}$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % epsilon2_hat
+% subplot(2,4,2)
+% plot(t,x_out(:,15),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\hat{\varepsilon}_{2}$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % epsilon3_hat
+% subplot(2,4,3)
+% plot(t,x_out(:,16),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\hat{\varepsilon}_{3}$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % eta_hat
+% subplot(2,4,4)
+% plot(t,x_out(:,17),'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\hat{\eta}$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % q_hat_norm
+% subplot(2,4,5)
+% plot(t,q_hat_norm,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('$\hat{\varepsilon}^{T}\hat{\varepsilon}+\hat{\eta}^{2}-1$','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % yaw_IN_err
+% subplot(2,4,6)
+% plot(t,yaw_IN_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('INS Yaw Error ($\psi^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % pitch_IN_err
+% subplot(2,4,7)
+% plot(t,pitch_IN_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('INS Pitch Error ($\theta^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % roll_IN_err
+% subplot(2,4,8)
+% plot(t,roll_IN_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('INS Roll Error ($\phi^{IN}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % TRIAD errors
+% % yaw_TRIAD_err
+% figure('Position',[100 100 1200 600])
+% subplot(1,3,1)
+% plot(t,yaw_TRIAD_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('TRIAD Yaw Error ($\psi^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % pitch_TRIAD_err
+% subplot(1,3,2)
+% plot(t,pitch_TRIAD_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('TRIAD Pitch Error ($\theta^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
+% 
+% % roll_TRIAD_err
+% subplot(1,3,3)
+% plot(t,roll_TRIAD_err,'Linewidth',line_width);
+% hold on
+% xlabel('Time (s)','fontsize',font_size,'Interpreter','latex');
+% ylabel('TRIAD Roll Error ($\phi^{TRIAD}_{err}, rad$)','fontsize',font_size,'Interpreter','latex');
+% set(gca,'XMinorGrid','off','GridLineStyle','-','FontSize',line_size)
+% grid on
+% axis square
--- simulation/run_simulation.m
+++ simulation/run_simulation.m
@@ -17,14 +17,18 @@ const_struct % All constants in one file.
 % Translational
 r_0    = [0, 0, C.Rs]';    % Initial position [x,y,z]'
 v_0    = [0, C.v, 0]';     % Initial velocity [xdot,ydot,zdot]'
+
 % Rotational
-epsilon_0 = zeros(3,1);        % Initial epsilon
-eta_0     = 1;                 % Initial eta
-% omega_0   = [0.08; -0.1; 0.3]; % Initial omega (rad/s)
-omega_0   = [0; 0; 0]; % Initial omega (rad/s)
+% epsilon_0 = zeros(3,1);         % Initial epsilon
+% eta_0     = 1;                  % Initial eta
+% omega_0   = [0; 0; 0];          % Initial omega (rad/s)
+epsilon_0 = (1/sqrt(6))*[1;1;1]; % Initial epsilon
+eta_0     = 1/sqrt(2);           % Initial eta
+omega_0   = [0.08; -0.1; 0.3];   % Initial omega (rad/s)
+
 % Attitude estimate
-epsilon_hat_0 = zeros(3,1);        % Initial epsilon
-eta_hat_0     = 1;                 % Initial eta
+epsilon_hat_0 = epsilon_0;     % Initial epsilon estimate
+eta_hat_0     = eta_0;         % Initial eta estimate
 
 % Combined Initial Conditions
 x = [r_0', v_0', epsilon_0', eta_0, omega_0', epsilon_hat_0', eta_hat_0]'; 
@@ -33,8 +37,9 @@ x = [r_0', v_0', epsilon_0', eta_0, omega_0', epsilon_hat_0', eta_hat_0]';
 % Simulation time.
 t_0 = 0; % s
 % t_max = .75*C.day; % s
-t_max = C.day; % s
-t_div = 1001; % number of steps to divide the time series into.
+% t_max = C.day; % s
+t_max = C.min; % s
+t_div = 101; % number of steps to divide the time series into.
 t_span = linspace(t_0,t_max,t_div); % Total simulation time.
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -59,4 +64,4 @@ plot_script_v2
 
 % %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % Save all the data. (You never know when you'll need it again.)
-save '..\simulation\output\part_5_full_orbit'
+save '..\simulation\output\part_6_q2_a'
